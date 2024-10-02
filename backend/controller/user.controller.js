2dd74dff76b797cdba1a1576eb4f8ffb392735d6
Harshit Shankhdhar <harshitshankhdhar01@gmail.com>
2024-10-03 03:42:28 +0530

    Add zod validations

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,19 +1,33 @@
 import User from "../model/user.model.js";
+import {z} from "zod"; // used for schema validation
+
+// zod validation
+const userValidation = z.object({
+    username: z.string().min(3,{message: 'Username must have at least 3 characters'}),
+    email: z.string().email({message: 'Invalid email', required: true}),
+    password: z.string().min(8,{message: 'Password must have at least 8 characters', required: true}),
+});
 
 export const register = async (req,res)=>{
     console.log(`Signup function called`);
-    const email = req.body.email;
+
+    const validationResult = userValidation.safeParse(req.body);
+    if(!validationResult.success) {
+        const errorMessage = validationResult.error.errors.map((err) => err.message);
+        return res.status(400).json({
+            message: "Invalid input",
+            errors: errorMessage
+        });
+    }
+
+    const { username, email, password} = req.body;
     const existingUser = await User.findOne({email});
     if(existingUser) {
-        res.status(200).json({
+        res.status(400).json({
             message: "User already exists"
         })
     };
-    const user = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    });
+    const user = new User({ username, email, password});
     try {
         const savedUser = await user.save();
         res.status(201).json({
@@ -32,8 +46,8 @@ export const login = async(req,res)=>{
     try {
     const email = req.body.email;
     const user = await User.findOne({email});
-    if(!user || !user.password.match(req.body.password)) {
-        res.status(200).json({message: "User does not exist"})
+    if(!user || !user.password === req.body.password) {
+        res.status(400).json({message: "User does not exist"})
     }
     res.status(200).json({
         message: "sign-in successful"
